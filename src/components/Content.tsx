566e3874ca44747c160f42841a54f174f77b007d
Gabriel Cortez <gabrielclformiga@gmail.com>
2021-03-15 16:37:02 -0300

    Chellenge Completed!

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,19 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+
+export function Content(props) {
+  return (
+  <div className="container">
+    <header>
+      <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+    </header>
+
+    <main>
+      <div className="movies-list">
+        {props.movies.map(movie => (
+          <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  </div>
+  );
 }
